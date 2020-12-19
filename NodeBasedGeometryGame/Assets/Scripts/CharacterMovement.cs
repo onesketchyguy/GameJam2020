6c1eb4dc7642528ee994e054aa430c808bf2a6bf
Pranish Kotha <pranishkotha@gmail.com>
2020-12-18 17:28:43 -0800

    Character Movement Update 2

--- NodeBasedGeometryGame/Assets/Scripts/CharacterMovement.cs
+++ NodeBasedGeometryGame/Assets/Scripts/CharacterMovement.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class CharacterMovement : MonoBehaviour
 {
     public float speed = 5;
+    public float jumpSpeed = 5;
+    public Rigidbody2D rigidBody;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,10 +16,16 @@ public class CharacterMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Vector3 input = new Vector3(Input.GetAxis("Horizontal")*speed,1,1);
-        Vector3 velocity = new Vector3();
-        velocity.x += input.x * Time.deltaTime;
-        transform.position += velocity;
+        Vector3 input = new Vector3(Input.GetAxis("Horizontal")*speed,Input.GetAxis("Vertical"),1);
+        Vector3 velocity = rigidBody.velocity;
+        if (input.y !=0)
+        {
+            rigidBody.AddForce(Vector2.up * jumpSpeed);
+        }
+
+
+        velocity.x = input.x * Time.deltaTime;
+        rigidBody.velocity = velocity;
 
     }
 }
