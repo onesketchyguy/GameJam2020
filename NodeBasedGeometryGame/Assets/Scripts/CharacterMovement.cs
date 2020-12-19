8e722a11ff56db36eaddcd0869a7e1638f46074c
onesketchyguy <lowebros9812@gmail.com>
2020-12-18 18:05:48 -0800

    Added animations

--- NodeBasedGeometryGame/Assets/Scripts/CharacterMovement.cs
+++ NodeBasedGeometryGame/Assets/Scripts/CharacterMovement.cs
@@ -7,25 +7,50 @@ public class CharacterMovement : MonoBehaviour
     public float speed = 5;
     public float jumpSpeed = 5;
     public Rigidbody2D rigidBody;
+
+    public float groundCheckOffset = 0.1f;
+    public float groundCheckDistance = 0.2f;
+    public LayerMask ground;
+
+    [Range(0.001f, 1)]
+    public float CoyoteTime = 0.24f;
+
+    private float coyoteTime = 1;
+
     // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
-        
     }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        Vector3 input = new Vector3(Input.GetAxis("Horizontal")*speed,Input.GetAxis("Vertical"),1);
-        Vector3 velocity = rigidBody.velocity;
-        if (input.y !=0)
-        {
-            rigidBody.AddForce(Vector2.up * jumpSpeed);
-        }
+        coyoteTime -= Time.deltaTime;
 
+        Vector3 input = new Vector3(Input.GetAxis("Horizontal") * speed, Input.GetAxis("Vertical"), 1);
+        Vector3 velocity = rigidBody.velocity;
+
+        if (input.y != 0 && GroundCheck()) rigidBody.AddForce(Vector2.up * jumpSpeed);
 
         velocity.x = input.x * Time.deltaTime;
         rigidBody.velocity = velocity;
+    }
 
+    private bool GroundCheck()
+    {
+        RaycastHit2D hitInfo = Physics2D.Raycast(transform.position - Vector3.up * groundCheckOffset, -Vector3.up, groundCheckDistance, ground);
+
+        coyoteTime = (hitInfo.transform != null) ? CoyoteTime : coyoteTime;
+
+        return (hitInfo.transform != null);
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = GroundCheck() ? Color.white : Color.red;
+
+        var start = transform.position - Vector3.up * groundCheckOffset;
+
+        Gizmos.DrawLine(start, start - Vector3.up * groundCheckDistance);
     }
 }
---
+++ NodeBasedGeometryGame/Assets/Scripts/PlayerAnimator.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class PlayerAnimator : MonoBehaviour
+{
+    public Animator animator;
+    public CharacterMovement controller;
+
+    public SpriteRenderer renderer;
+
+    private const float WEIGHT = 0.1f;
+
+    private void Update()
+    {
+        if (controller.rigidBody.velocity.x > WEIGHT)
+            renderer.flipX = false;
+        else if (controller.rigidBody.velocity.x < -WEIGHT)
+            renderer.flipX = true;
+
+        animator.SetFloat("SpeedX", Mathf.Abs(controller.rigidBody.velocity.x));
+        animator.SetFloat("SpeedY", controller.rigidBody.velocity.y);
+    }
+}
