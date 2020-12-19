ed07f38ef838951050005f1cdcd1046cd9d961e0
Pranish Kotha <pranishkotha@gmail.com>
2020-12-18 17:09:51 -0800

    Character Physics Update 1

---
+++ NodeBasedGeometryGame/Assets/Scripts/CharacterMovement.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMovement : MonoBehaviour
+{
+    public float speed = 5;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 input = new Vector3(Input.GetAxis("Horizontal")*speed,1,1);
+        Vector3 velocity = new Vector3();
+        velocity.x += input.x * Time.deltaTime;
+        transform.position += velocity;
+
+    }
+}
